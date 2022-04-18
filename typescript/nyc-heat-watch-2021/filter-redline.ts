3aebf928b2c29b29dc4a77eae1ec13926ee0c7d9
Jason Biegel <me@jasonbiegel.com>
2022-04-17 22:31:36 -0400

    remove deplicate redline

--- typescript/nyc-heat-watch-2021/filter-redline.ts
+++ typescript/nyc-heat-watch-2021/filter-redline.ts
@@ -1,12 +1,23 @@
 import { Feature, FeatureCollection, intersect, MultiPolygon, Polygon } from '@turf/turf';
 
 export default function filterRedline(areasOfInterest: FeatureCollection, redlineDataset: FeatureCollection): Array<Feature> {
-  const overlappedRedline = [];
+  const overlappedRedline: Array<Feature> = [];
 
   for (const redlineData of redlineDataset.features) {
     for (const tract of areasOfInterest.features) {
       if (intersect(redlineData as Feature<Polygon | MultiPolygon>, tract as Feature<Polygon | MultiPolygon>)) {
-        overlappedRedline.push(redlineData);
+        let alreadyFound = false;
+
+        for(const foundRedline of overlappedRedline) {
+          if(intersect(foundRedline as Feature<Polygon | MultiPolygon>, redlineData as Feature<Polygon | MultiPolygon>)) {
+            alreadyFound = true;
+            break;
+          }
+        }
+
+        if(!alreadyFound) {
+          overlappedRedline.push(redlineData);
+        }
       }
     }
   }
